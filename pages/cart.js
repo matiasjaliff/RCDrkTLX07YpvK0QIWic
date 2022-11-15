19d08456a27a9a69661f4dfb8cef052c03a70677
Matias Jaliff <matiasjaliff@gmail.com>
2022-11-15 09:44:43 -0300

    add credit component to cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 import CartCard from "../components/common/CartCard";
+import Credit from "../components/common/Credit";
 
 import styles from "./cart.module.css";
 
 export default function cart({ order }) {
+  const [credit, setCredit] = useState(2000);
+  
   return (
-    <div className={styles.container}>
-      {order.map((item, index) => (
-        <CartCard key={index} item={item} />
-      ))}
-    </div>
+    <>
+      <div className={styles.container}>
+        {order.map((item, index) => (
+          <CartCard key={index} item={item} />
+        ))}
+      </div>
+      <Credit credit={credit} setCredit={setCredit} />
+    </>
   );
 }
